5dfc43a5f6e2d09b7df5fbc579e4bbbf3d3d1af9
Cody Moore <cmoore109@liberty.edu>
2022-08-29 16:16:23 -0400

    changed how value for highlights are retrieved in EditorController

--- test/highlights/highlights_test.dart
+++ test/highlights/highlights_test.dart
@@ -19,7 +19,7 @@ void main() {
   late EditorController editorController;
   setUp(() {
     document = DocumentM.fromJson(jsonDecode(SIMPLE_TEXT_MOCK));
-    editorController = EditorController(document: document);
+    editorController = EditorController(document: document, highlights: []);
   });
 
   group('Highlights', () {

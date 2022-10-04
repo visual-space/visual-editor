cefa9c08c7b0720d9d90fd91c81574ac1e531b6a
Cody Moore <cmoore109@liberty.edu>
2022-10-04 17:53:46 -0400

    test with updated getHighlights method

--- test/highlights/highlights_test.dart
+++ test/highlights/highlights_test.dart
@@ -29,12 +29,12 @@ void main() {
           TextSelection(baseOffset: 5, extentOffset: 10), ChangeSource.LOCAL);
       editorController.addHighlight(HighlightM(
           textSelection: TextSelection(baseOffset: 5, extentOffset: 10)));
-      expect(editorController.highlights, [
+      expect(editorController.getHighlights(), [
         HighlightM(
             textSelection: TextSelection(baseOffset: 5, extentOffset: 10))
       ]);
     });
-    test('Adds multiple highlights, no overlap', () {
+    test('Adds multiple getHighlights(), no overlap', () {
       editorController.updateSelection(
           TextSelection(baseOffset: 5, extentOffset: 10), ChangeSource.LOCAL);
       editorController.addHighlight(HighlightM(
@@ -46,7 +46,7 @@ void main() {
       editorController.addHighlight(HighlightM(
           textSelection: TextSelection(baseOffset: 25, extentOffset: 50),
           color: Colors.blue));
-      expect(editorController.highlights, [
+      expect(editorController.getHighlights(), [
         HighlightM(
             textSelection: TextSelection(baseOffset: 5, extentOffset: 10),
             color: Colors.green),
@@ -58,7 +58,7 @@ void main() {
             color: Colors.blue)
       ]);
     });
-    test('Adds multiple highlights, overlap', () {
+    test('Adds multiple getHighlights(), overlap', () {
       editorController.updateSelection(
           TextSelection(baseOffset: 5, extentOffset: 10), ChangeSource.LOCAL);
       editorController.addHighlight(HighlightM(
@@ -72,7 +72,7 @@ void main() {
       editorController.addHighlight(HighlightM(
           textSelection: TextSelection(baseOffset: 25, extentOffset: 50),
           color: Colors.blue));
-      expect(editorController.highlights, [
+      expect(editorController.getHighlights(), [
         HighlightM(
             textSelection: TextSelection(baseOffset: 5, extentOffset: 15),
             color: Colors.green),
@@ -91,7 +91,7 @@ void main() {
           textSelection: TextSelection(baseOffset: 5, extentOffset: 10)));
       editorController.removeHighlight(HighlightM(
           textSelection: TextSelection(baseOffset: 5, extentOffset: 10)));
-      expect(editorController.highlights, []);
+      expect(editorController.getHighlights(), []);
     });
     test('Removes highlight via same highlight object', () {
       final highlight = HighlightM(
@@ -100,7 +100,7 @@ void main() {
           TextSelection(baseOffset: 5, extentOffset: 10), ChangeSource.LOCAL);
       editorController.addHighlight(highlight);
       editorController.removeHighlight(highlight);
-      expect(editorController.highlights, []);
+      expect(editorController.getHighlights(), []);
     });
     test('Removes partial highlight above highlight extent via selection range',
         () {
@@ -112,7 +112,7 @@ void main() {
       editorController.removeHighlightInRange(
           editorController.selection.baseOffset,
           editorController.selection.extentOffset);
-      expect(editorController.highlights, [
+      expect(editorController.getHighlights(), [
         HighlightM(textSelection: TextSelection(baseOffset: 5, extentOffset: 7))
       ]);
     });
@@ -126,13 +126,13 @@ void main() {
       editorController.removeHighlightInRange(
           editorController.selection.baseOffset,
           editorController.selection.extentOffset);
-      expect(editorController.highlights, [
+      expect(editorController.getHighlights(), [
         HighlightM(
             textSelection: TextSelection(baseOffset: 8, extentOffset: 10))
       ]);
     });
     test(
-        'Removes partial highlight inside highlight extent via selection range - case where two resultant highlights are required',
+        'Removes partial highlight inside highlight extent via selection range - case where two resultant getHighlights() are required',
         () {
       final highlight = HighlightM(
           textSelection: TextSelection(baseOffset: 5, extentOffset: 30));
@@ -142,7 +142,7 @@ void main() {
       editorController.removeHighlightInRange(
           editorController.selection.baseOffset,
           editorController.selection.extentOffset);
-      expect(editorController.highlights, [
+      expect(editorController.getHighlights(), [
         HighlightM(
             textSelection: TextSelection(baseOffset: 5, extentOffset: 10)),
         HighlightM(
@@ -158,9 +158,10 @@ void main() {
       editorController.removeHighlightInRange(
           editorController.selection.baseOffset,
           editorController.selection.extentOffset);
-      expect(editorController.highlights, []);
+      expect(editorController.getHighlights(), []);
     });
-    test('Removes first highlight retrieved when two highlights overlap', () {
+    test('Removes first highlight retrieved when two getHighlights() overlap',
+        () {
       final highlight = HighlightM(
           textSelection: TextSelection(baseOffset: 5, extentOffset: 10),
           color: Colors.green);
@@ -171,14 +172,14 @@ void main() {
           TextSelection(baseOffset: 2, extentOffset: 15), ChangeSource.LOCAL);
       editorController.addHighlight(highlight);
       editorController.addHighlight(highlight2);
-      // NOTE this method in the HighlightsState class uses the same logic as the above, which manages the removal of highlights smaller than the full size of the
+      // NOTE this method in the HighlightsState class uses the same logic as the above, which manages the removal of getHighlights() smaller than the full size of the
       // highlight. If those work, then this should work for any case in which they work.
       editorController.removeFirstHighlightInRange(
           editorController.selection.baseOffset,
           editorController.selection.extentOffset);
-      expect(editorController.highlights, [highlight2]);
+      expect(editorController.getHighlights(), [highlight2]);
     });
-    test('remove all highlights', () {
+    test('remove all getHighlights()', () {
       final highlight = HighlightM(
           textSelection: TextSelection(baseOffset: 5, extentOffset: 10),
           color: Colors.green);
@@ -190,8 +191,8 @@ void main() {
       editorController.addHighlight(highlight);
       editorController.addHighlight(highlight2);
       editorController.removeAllHighlights();
-      expect(editorController.highlights, []);
+      expect(editorController.getHighlights(), []);
     });
-    // test('Hovers highlights', () {});
+    // test('Hovers getHighlights()', () {});
   });
 }

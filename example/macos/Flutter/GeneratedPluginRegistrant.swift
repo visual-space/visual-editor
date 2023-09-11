84875c9ee33d23767eb491e6b6ec2b8fcd6972c4
Luís Arteiro <lucho14garfield@gmail.com>
2023-09-11 18:04:59 +0100

    236 - Updating dependencies on package and example app
    - Updated `i18n_extensions` and fixed gradle files to make it run. Also repaired iOS files (podfile) so the example app could run.
    - Updated `image_picker`
    - Updated `string_validator`
    - Upgraded `device_info_plus`. With this upgrade, also updated the `file_picker` on the example app otherwise dependency versioning wouldn't work. Also deleted the generated `generated_plugin_registrant.dart`, as it's not needed. See https://github.com/fluttercommunity/plus_plugins/issues/1274.
    - left `pedantic` on, even though `flutter_lint` is the default way now. This is because I don't know the exact rules and if this was desirable for your team.

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,7 @@
 import FlutterMacOS
 import Foundation
 
-import device_info_plus_macos
+import device_info_plus
 import file_selector_macos
 import path_provider_foundation
 import url_launcher_macos

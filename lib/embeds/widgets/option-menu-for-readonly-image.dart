081cca69960c04dda67abc08234ff87f5ea363bd
Luís Arteiro <lucho14garfield@gmail.com>
2023-09-13 15:44:30 +0100

    chore: Replacing `gallery_saver` with `gal`. #238

--- lib/embeds/widgets/option-menu-for-readonly-image.dart
+++ lib/embeds/widgets/option-menu-for-readonly-image.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:gallery_saver/gallery_saver.dart';
+import 'package:gal/gal.dart';
+import 'package:path/path.dart';
 
 import '../../shared/state/editor.state.dart';
 import '../../shared/translations/toolbar.i18n.dart';
@@ -54,20 +58,44 @@ class OptionMenuForReadOnlyImage extends StatelessWidget {
         icon: Icons.save,
         color: Colors.greenAccent,
         text: 'Save'.i18n,
-        onPressed: () {
+        onPressed: () async {
+          // Function for checking if the given image url is a local file or a link from the web.
+          // Logic is the same as from `gallery_saver` in https://github.com/CarnegieTechnologies/gallery_saver/blob/45017dbf88bebe785f498f0ddfb30d75409c436e/lib/files.dart#L23-L27.
+          bool isLocalFilePath(String path) {
+            final uri = Uri.parse(path);
+            return !uri.scheme.contains('http');
+          }
+
+          // Get image path or URL input
           final _imageUrl = _mediaLoaderService.appendFileExtensionToImageUrl(
             imageUrl,
           );
-          GallerySaver.saveImage(_imageUrl).then(
-            (_) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text('Saved'.i18n),
-                ),
-              );
-              Navigator.pop(context);
-            },
-          );
+
+          // Define image path directory where the image will be saved into
+          final imagePath = '${Directory.systemTemp.path}/${basename(_imageUrl)}';
+
+          try {
+            // We check if the path is a local file path. If so, we only save it to the gallery.
+            if (isLocalFilePath(_imageUrl)) {
+              await Gal.putImage(imagePath);
+            }
+            // If it's an URL, we need to download it locally and save it afterwards
+            else {
+              await Dio().download(_imageUrl, imagePath);
+              await Gal.putImage(imagePath);
+            }
+
+            // Show a snackbar stating the image was successfully saved
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Saved'.i18n),
+              ),
+            );
+            Navigator.pop(context);
+          } catch (e) {
+            // TODO: handle error properly
+            print(e);
+          }
         },
       );
 
